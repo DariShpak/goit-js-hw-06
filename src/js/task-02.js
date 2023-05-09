be6888d5e511f64cdc8721a9d4264246428c3bf4
Daria Shpak <darya.shpak@gmail.com>
2023-05-09 19:55:20 +0300

    Second task completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+]
+
+const ingredientItems = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li")
+  listItem.textContent = ingredient
+  listItem.classList.add("item")
+  return listItem
+})
+
+const listOfIngredients = document.querySelector("ul#ingredients")
+listOfIngredients.append(...ingredientItems)
+
