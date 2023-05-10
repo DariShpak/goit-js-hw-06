72ee15a4d989f530b4f7718c9e8dac25b940bc3b
Daria Shpak <darya.shpak@gmail.com>
2023-05-10 14:29:50 +0300

    Completed 4 task

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,26 @@
+
+
+const decrBtn = document.querySelector("button[data-action='decrement']")
+console.log(decrBtn.dataset.action)
+
+const incrBtn = document.querySelector("button[data-action='increment']")
+console.log(incrBtn.dataset.action)
+
+const value = document.querySelector('#value')
+console.log(value.textContent)
+
 let counterValue = 0;
+
+decrBtn.addEventListener("click", decrClick)
+incrBtn.addEventListener("click", incrClick)
+
+function decrClick(event) {
+  counterValue -= 1
+  value.textContent = counterValue
+}
+
+function incrClick(event) {
+  counterValue += 1
+  value.textContent = counterValue
+}
+
