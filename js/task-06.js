d94ec7f2d8368e4d97613b0de5e77770cd2126c8
Daria Shpak <darya.shpak@gmail.com>
2023-05-12 00:43:05 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,19 @@
 const inputEl = document.querySelector("input")
 
-const minDataLength = inputEl.getAttribute("data-length")
-console.log(minDataLength)
+const minDataLength = Number(inputEl.getAttribute("data-length"))
 
 inputEl.addEventListener("blur", onBlur)
 
 function onBlur(event) {
-  if (inputEl.value.length == minDataLength) {
+  const outputLength = event.currentTarget.value.trim().length
+  console.log(outputLength)
+
+  if (outputLength === minDataLength) {
+    console.log(minDataLength)
     inputEl.classList.add("valid")
+     inputEl.classList.remove("invalid")
   } else {
     inputEl.classList.add("invalid")
-  }
+    inputEl.classList.remove("valid")
+  } 
 }
