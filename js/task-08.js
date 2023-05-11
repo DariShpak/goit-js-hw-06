c68d87b7176cb685495bc20614c14f70341478a3
Daria Shpak <darya.shpak@gmail.com>
2023-05-11 22:43:06 +0300

    Completed 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form")
+console.dir(form)
 
+form.addEventListener("submit", onFormSubmit)
+
+function onFormSubmit(event) {
+  event.preventDefault()
+  const {email, password} = event.currentTarget.elements
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  }
+  console.log(formData)
+  
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!")
+  }
+
+  event.currentTarget.reset()
+}
