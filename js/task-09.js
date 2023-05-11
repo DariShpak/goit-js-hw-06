1ba0bbd50fe72427d383231098fadfb43de64084
Daria Shpak <darya.shpak@gmail.com>
2023-05-11 23:32:12 +0300

    Completed 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const body = document.querySelector("body")
+const bgColorName = document.querySelector(".color")
+
+const changeColorBtn = document.querySelector(".change-color")
+changeColorBtn.addEventListener("click", onBtnClick)
+
+function onBtnClick() {
+  body.style.backgroundColor = getRandomHexColor()
+  bgColorName.textContent = getRandomHexColor()
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, 0)}`
 }
