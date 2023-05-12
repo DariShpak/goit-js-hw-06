a948aca01a23c758d24a6cc4fd64c82724ea17c6
Daria Shpak <darya.shpak@gmail.com>
2023-05-12 14:27:35 +0300

    Completed 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,38 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, 0)}`
 }
+
+const createBtn = document.querySelector("[data-create]")
+const destroyBtn = document.querySelector("[data-destroy]")
+const input = document.querySelector("input")
+
+function createBoxes(amount) {
+  const boxes = document.querySelector("#boxes")
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div")
+    box.style.backgroundColor = getRandomHexColor()
+    box.style.width = `${30 + i * 10}px`
+    box.style.height = `${30 + i * 10}px`
+    boxes.appendChild(box)
+  }
+}
+
+function destroyBoxes() {
+  const boxes = document.querySelector("#boxes")
+  boxes.innerHTML = ""
+}
+
+createBtn.addEventListener("click", () => {
+  createBoxes(input.value)
+})
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes()
+})
+
+
+
